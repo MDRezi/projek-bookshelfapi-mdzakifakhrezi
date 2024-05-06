1143e10f9759938ffe5f633c0f483f268f995319
Dzaki <12150114774@students.uin-suska.ac.id>
2024-05-06 17:19:58 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,27 @@
+//server.js
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  try {
+    const server = Hapi.server({
+      port: 9000,
+      host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+      routes: {
+        cors: {
+          origin: ['*'],
+        },
+      },
+    });
+
+    server.route(routes);
+
+    await server.start();
+    console.log(`Server telah berhasil dijalankan !, silahkan masuk pada port berikut => ${server.info.uri}`);
+  } catch (error) {
+    console.error('Maaf terjadi kesalahan pada saat menjalankan server:', error);
+    process.exit(1);
+  }
+};
+
+init();
